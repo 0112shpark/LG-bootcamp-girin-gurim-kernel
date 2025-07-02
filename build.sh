ac4bab7cc925c24e31f1389eae020f5987731090
inniestar97 <tkddls0614@gmail.com>
2025-07-02 14:33:59 +0900

    production added under ./build_product directory

--- build.sh
+++ build.sh
@@ -21,7 +21,25 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
-echo "Image loclation: $(pwd)/arch/arm64/boot/Image"
-echo "Device Tree Blob loclation: $(pwd)/arch/arm64/boot/dts/broadcom/wt2837.dtb"
-echo ""
-echo "Copy the Image and Device Tree Blob to tftp servere directory"
+if cp "$(pwd)/arch/arm64/boot/Image" "$(pwd)/build_product" ; then
+    echo "Copy Image to build_product directory successful"
+else
+    echo "Copy Image to build_product directory failed"
+    exit 1
+fi
+
+if cp "$(pwd)/arch/arm64/boot/dts/broadcom/wt2837.dtb" "$(pwd)/build_product" ; then
+    echo "Copy Device Tree Blob to build_product directory successful"
+else
+    echo "Copy Device Tree Blob to build_product directory failed"
+    exit 1
+fi
+
+RED='\033[0;31m'
+YELLOW='\033[0;33m'
+NC='\033[0m' # No Color
+
+printf "Image loclation: ${RED}$(pwd)/build_product/Image${NC}\n"
+printf "Device Tree Blob loclation: ${RED}$(pwd)/build_product/wt2837.dtb${NC}\n"
+printf "\n\n"
+printf "${YELLOW}Copy the Image and Device Tree Blob to tftp server directory${NC}\n"
